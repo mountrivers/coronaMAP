1cdf2f9bb26092486d4387ae37e4bf161cc1b99f
mountrivers <36880919+mountrivers@users.noreply.github.com>
2020-03-08 21:28:22 +0900

    NewsActivity 파기 고지
    주석으로 현재 사용하지 않는 엑티비티 명시

--- app/src/main/java/com/sanha/coronamap/ACTIVITY_NEWS/NewsActivity.java
+++ app/src/main/java/com/sanha/coronamap/ACTIVITY_NEWS/NewsActivity.java
@@ -26,7 +26,19 @@ import org.jsoup.select.Elements;
 import java.io.IOException;
 
 public class NewsActivity extends AppCompatActivity {
+    /*
+     이 파일은 현재 사용 되지 않는 파일입니다.
 
+     변천사 )
+     직접 뉴스를 골라 사용자들에게 뿌려주는 방식
+     (Firebase 사용, 관리자 프로그램에서 링크, 날짜, 제목 전송 -> 사용자들이 뉴스 내용을 받는 방식.)
+
+     => 다음 뉴스 "코로나19" 이슈 카테고리 에서 크롤링 해오는 방식
+
+     => 현재는 NewsRoomActivity 로 옮겨, 두개의 프래그먼트로 하나는 지역별, 하나는 기존의 뉴스 카테고리를 보여주는
+     기능으로 변경. 
+
+     */
     private ListView news_viewr;
     private FirebaseDatabase database = FirebaseDatabase.getInstance();
     private DatabaseReference databaseReference = database.getReference();
